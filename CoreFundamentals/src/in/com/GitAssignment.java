9e4f57d66b599137dfe55bd67bf663c867673422
subhransusekharmohanty <mohantysubhransu1990@gmail.com>
2023-02-05 17:16:14 +0530

    second commit

--- CoreFundamentals/src/in/com/GitAssignment.java
+++ CoreFundamentals/src/in/com/GitAssignment.java
@@ -5,7 +5,7 @@ public class GitAssignment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("First commit");
+		System.out.println("Second commit");
 
 	}
 
