a7d5d0763aa18deb2053cb8f76118a80dbee528d
subhransusekharmohanty <mohantysubhransu1990@gmail.com>
2023-02-05 18:05:14 +0530

    subhamcom

--- CoreFundamentals/src/in/com/GitAssignment.java
+++ CoreFundamentals/src/in/com/GitAssignment.java
@@ -6,7 +6,7 @@ public class GitAssignment {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Second commit");
-		System.out.println("Verify default ");
+		System.out.println("Verify default erte ");
 
 	}
 }
