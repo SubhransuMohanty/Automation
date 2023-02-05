0e7c9f25c168a5404975d924763b07f4c2f36fd5
subhransusekharmohanty <mohantysubhransu1990@gmail.com>
2023-02-05 16:52:18 +0530

    first commit

---
+++ CoreFundamentals/src/in/com/GitAssignment.java
@@ -0,0 +1,12 @@
+package in.com;
+
+public class GitAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("First commit");
+
+	}
+
+}
